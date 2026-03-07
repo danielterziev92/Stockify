7f70e4beb5c8ed4d39efb85a065125a0f689665e
Daniel Terziev <daniel.st.terziev@gmail.com>
2026-03-07 18:54:46 +0200

    Extract repositories for PartnerCategory and ProductCategory with a shared generic base using Category hierarchy

---
+++ catalog/src/main/java/com/stockify/catalog/repository/PartnerCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.stockify.catalog.repository;
+
+import com.stockify.catalog.model.PartnerCategory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PartnerCategoryRepository extends CategoryRepository<PartnerCategory> {
+}
