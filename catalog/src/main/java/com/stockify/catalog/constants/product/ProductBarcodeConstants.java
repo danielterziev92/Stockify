4b4658df15eadc2d4b13815f0b30779b6e00952a
Daniel Terziev <daniel.st.terziev@gmail.com>
2026-03-14 17:26:58 +0200

    Introduce `ProductBarcode` entity and migration script
    - Add `ProductBarcode` entity to represent barcode information with fields `id`, `version`, `value`, `type`, and a reference to `ProductVariant`.
    - Create `ProductBarcodeConstants` to define length constraints for `value` and `type`.
    - Add V15 migration script to define the `product_barcodes` table with unique constraints, indexes, and foreign key relationships.

---
+++ catalog/src/main/java/com/stockify/catalog/constants/product/ProductBarcodeConstants.java
@@ -0,0 +1,11 @@
+package com.stockify.catalog.constants.product;
+
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ProductBarcodeConstants {
+    public static final int VALUE_MAX_LENGTH = 150;
+    public static final int TYPE_MAX_LENGTH = 10;
+}
