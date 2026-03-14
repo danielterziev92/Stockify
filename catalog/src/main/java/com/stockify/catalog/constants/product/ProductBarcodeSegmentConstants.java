405f6741091c2d3d7917215d9b550b68787a01ea
Daniel Terziev <daniel.st.terziev@gmail.com>
2026-03-14 18:00:33 +0200

    Introduce `ProductBarcodeTemplate` and `ProductBarcodeSegment` entities with migration scripts
    - Add `ProductBarcodeTemplate` to represent reusable barcode templates.
    - Add `ProductBarcodeSegment` for defining individual segments within a barcode template.
    - Create constant classes for field validation.
    - Add V16 and V17 migration scripts to define respective tables with constraints and relationships.

---
+++ catalog/src/main/java/com/stockify/catalog/constants/product/ProductBarcodeSegmentConstants.java
@@ -0,0 +1,10 @@
+package com.stockify.catalog.constants.product;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ProductBarcodeSegmentConstants {
+    public static final int NAME_MAX_LENGTH = 30;
+    public static final int SEGMENT_MAX_LENGTH = 20;
+}
---
+++ catalog/src/main/java/com/stockify/catalog/model/product/ProductBarcodeTemplate.java
@@ -0,0 +1,37 @@
+package com.stockify.catalog.model.product;
+
+import com.stockify.catalog.constants.product.ProductBarcodeTemplateConstants;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(
+        name = "product_barcode_templates",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_product_barcode_template_prefix", columnNames = "prefix")
+        }
+)
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductBarcodeTemplate {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @Version
+    @Column(nullable = false)
+    private Long version;
+
+    @Column(length = ProductBarcodeTemplateConstants.NAME_MAX_LENGTH, nullable = false)
+    private String name;
+
+    @Column(length = ProductBarcodeTemplateConstants.PREFIX_MAX_LENGTH, nullable = false)
+    private String prefix;
+
+    @Column(nullable = false)
+    private Integer length;
+}
