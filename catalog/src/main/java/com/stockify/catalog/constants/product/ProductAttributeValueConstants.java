eb23befead78ce3166935a4df590adc26329dbaf
Daniel Terziev <daniel.st.terziev@gmail.com>
2026-03-14 15:54:28 +0200

    Introduce `ProductAttributeValue` entity and migration script
    - Add `ProductAttributeValue` entity with fields `id`, `version`, `value`, `abbreviation`, and a reference to `ProductAttributeKey`.
    - Create `ProductAttributeValueConstants` to define value and abbreviation length constraints.
    - Add V9 migration script to define the `product_attribute_values` table with indexes and foreign key constraints.

---
+++ catalog/src/main/java/com/stockify/catalog/constants/product/ProductAttributeValueConstants.java
@@ -0,0 +1,12 @@
+package com.stockify.catalog.constants.product;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ProductAttributeValueConstants {
+
+    public static final int VALUE_MAX_LENGTH = 50;
+    public static final int ABBREVIATION_MAX_LENGTH = 10;
+
+}
