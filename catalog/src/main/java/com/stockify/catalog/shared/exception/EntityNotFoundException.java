9ee2c9be201cbecd6c38319182e3def6dbbb027c
Daniel Terziev <daniel.st.terziev@gmail.com>
2026-04-10 23:29:12 +0300

    Remove catalog and user modules
    - Delete `catalog` and `user` modules along with their associated configurations, Docker setup, and exception classes.
    - Clean up references in `.gitignore`, `.gitattributes`, and Maven wrapper files.
    - Remove shared utilities and value object interfaces used by these modules.

--- catalog/src/main/java/com/stockify/catalog/shared/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.stockify.catalog.shared.exception;
-
-public class EntityNotFoundException extends DomainException {
-    public EntityNotFoundException(String messageCode, Object... args) {
-        super(messageCode, args);
-    }
-}
--- client/shared/domain/IValueObject.ts
+++
@@ -1,5 +0,0 @@
-export interface IValueObject<T> {
-    getValue(): T;
-    equals(other: IValueObject<T>): boolean;
-    toString(): string;
-}
